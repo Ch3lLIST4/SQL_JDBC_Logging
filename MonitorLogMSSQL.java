5293c1880e77aec983cca9e7478937e58056f916
Ch3lLIST4 <56858779+Ch3lLIST4@users.noreply.github.com>
2020-09-24 16:45:58 +0700

    Update MonitorLogMSSQL.java

--- MonitorLogMSSQL.java
+++ MonitorLogMSSQL.java
@@ -16,11 +16,15 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.util.Pair;
 
 /**
@@ -32,6 +36,8 @@ public class MonitorLogMSSQL {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
+    // purple for testing random
+    public static final String ANSI_PURPLE = "\u001B[35m";
     
     public static Connection getConnection(String ip_address, String port_number, 
             String instanceName, String databaseName, String username, String password) {
@@ -130,17 +136,48 @@ public class MonitorLogMSSQL {
                     
                     //first run
                     if (ran_once == false) { 
+                        
+                        //for each record
                         while (result.next()) {
                             String hash = result.getString("Hash").trim();
                             String time = result.getString("Time").trim();
                             String query = result.getString("Query").trim();
-                                         
+                            
+                            //count types of queries in an instance
+                            int type_count = 0;
+                            if (query.contains("INSERT")) {
+                                type_count++;        
+                            }
+                            if (query.contains("UPDATE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("DELETE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("TRUNCATE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("ALTER")) {
+                                type_count++;   
+                            }
+                            
+                            //detect types of query
                             if (query.contains("INSERT") || query.contains("UPDATE") || query.contains("DELETE") || 
                                     query.contains("TRUNCATE") || query.contains("ALTER")){
                                 
                                 Pair<String,String> pair = new Pair<>(hash, time);
+                                
+                                //if only one query instance -> remove weird pre-query
+                                if (type_count == 1) {
+                                    query = query.replaceFirst("\\(.*\\)INSERT", "INSERT")
+                                            .replaceFirst("\\(.*\\)UPDATE", "UPDATE")
+                                            .replaceFirst("\\(.*\\)DELETE", "DELETE")
+                                            .replaceFirst("\\(.*\\)TRUNCATE", "TRUNCATE")
+                                            .replaceFirst("\\(.*\\)ALTER", "ALTER");
+                                }                         
+                                
                                 query_list.put(pair,query);
-                                System.out.println(ANSI_RED + time + ANSI_RESET + " - " + query
+                                System.out.println(ANSI_RED + time + ANSI_RESET + " - " + query                   
                                         .replaceAll("INSERT", ANSI_BLUE + "INSERT" + ANSI_RESET)
                                         .replaceAll("UPDATE", ANSI_BLUE + "UPDATE" + ANSI_RESET)
                                         .replaceAll("DELETE", ANSI_BLUE + "DELETE" + ANSI_RESET)
@@ -150,8 +187,8 @@ public class MonitorLogMSSQL {
                         }
                         
                         ran_once = true;
-                        System.out.println("");
-                        TimeUnit.SECONDS.sleep(2);          
+                        System.out.println("\nThe end of existed cached logs.\n");
+                        TimeUnit.SECONDS.sleep(1);          
                     } 
                     //after first run loop
                     else if (ran_once == true) {
@@ -160,11 +197,38 @@ public class MonitorLogMSSQL {
                             String time = result.getString("Time").trim();
                             String query = result.getString("Query").trim();
                             
+                            //count types of queries in an instance
+                            int type_count = 0;
+                            if (query.contains("INSERT")) {
+                                type_count++;        
+                            }
+                            if (query.contains("UPDATE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("DELETE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("TRUNCATE")) {
+                                type_count++;   
+                            }
+                            if (query.contains("ALTER")) {
+                                type_count++;   
+                            }                      
+                            
                             Pair<String,String> pair = new Pair<>(hash, time);
                             // if time hasnt existed in the list
                             if (query_list.get(pair) == null) {
                                 if (query.contains("INSERT") || query.contains("UPDATE") || query.contains("DELETE") || 
                                     query.contains("TRUNCATE") || query.contains("ALTER")){
+                                    
+                                    //if only one query instance -> remove weird pre-query
+                                    if (type_count == 1) {
+                                        query = query.replaceFirst("\\(.*\\)INSERT", "INSERT")
+                                                .replaceFirst("\\(.*\\)UPDATE", "UPDATE")
+                                                .replaceFirst("\\(.*\\)DELETE", "DELETE")
+                                                .replaceFirst("\\(.*\\)TRUNCATE", "TRUNCATE")
+                                                .replaceFirst("\\(.*\\)ALTER", "ALTER");
+                                    }
                                     
                                     query_list.put(pair,query);
                                     System.out.println(ANSI_RED + time + ANSI_RESET + " - " + query
@@ -193,25 +257,71 @@ public class MonitorLogMSSQL {
             if (conn != null) {
                 while (true) {
                     int randomNum1 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
-                    int randomNum2 = ThreadLocalRandom.current().nextInt(1, 50 + 1);
-                    int randomNum3 = ThreadLocalRandom.current().nextInt(1, 10 + 1);
-
+                    int randomNum2 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    int randomNum3 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    
+                    int randomNum4 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    int randomNum5 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    int randomNum6 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    
+                    int randomNum7 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    int randomNum8 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    int randomNum9 = ThreadLocalRandom.current().nextInt(1, 500 + 1);
+                    
+                    //INSERT
                     String sql1 = "INSERT INTO Users (id, name, password, mail) " +
                         "VALUES ('7', " + randomNum1 + ", " + randomNum2 + ", " + randomNum3 +")";
-                    String sql3 = "UPDATE Users SET password = " + randomNum1 + " WHERE id = '7'";
-                    String sql2 = "DELETE FROM Users WHERE id = '7'";
-
+                    String sql2 = "INSERT INTO Users (id, name, password, mail) " +
+                        "VALUES ('7', " + randomNum4 + ", " + randomNum5 + ", " + randomNum6 +")";
+                    String sql3 = "INSERT INTO Users (id, name, password, mail) " +
+                        "VALUES ('7', " + randomNum7 + ", " + randomNum8 + ", " + randomNum9 +")";
+                    
                     PreparedStatement statement1 = conn.prepareStatement(sql1);
                     PreparedStatement statement2 = conn.prepareStatement(sql2);
                     PreparedStatement statement3 = conn.prepareStatement(sql3);
-
-                    statement1.executeUpdate();
-                    statement1.executeUpdate();
-                    statement1.executeUpdate();
-                    statement2.executeUpdate();
-                    statement3.executeUpdate();
                     
-                    TimeUnit.SECONDS.sleep(5); 
+                    //UPDATE
+                    String sql4 = "UPDATE Users SET password = " + randomNum1 + " WHERE id = '7'";
+                    
+                    PreparedStatement statement4 = conn.prepareStatement(sql4);
+                    
+                    //DELETE
+                    String sql5 = "DELETE FROM Users WHERE id = '7'";
+
+                    PreparedStatement statement5 = conn.prepareStatement(sql5);
+
+                    
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");  
+                    
+                    //Statement 1 INSERT
+                    LocalDateTime now = LocalDateTime.now();  
+                    System.out.println(ANSI_PURPLE + dtf.format(now) + " - " + sql1 + ANSI_RESET);  
+                    statement1.executeUpdate();
+                    TimeUnit.SECONDS.sleep(2); 
+                    
+                    //Statement 2 INSERT
+                    now = LocalDateTime.now();  
+                    System.out.println(ANSI_PURPLE + dtf.format(now) + " - " + sql2 + ANSI_RESET); 
+                    statement2.executeUpdate();
+                    TimeUnit.SECONDS.sleep(1);       
+                    
+                    //Statement 4 UPDATE
+                    now = LocalDateTime.now();  
+                    System.out.println(ANSI_PURPLE + dtf.format(now) + " - " + sql4 + ANSI_RESET); 
+                    statement4.executeUpdate();
+                    TimeUnit.SECONDS.sleep(2); 
+                    
+                    //Statement 5 DELETE
+                    now = LocalDateTime.now();  
+                    System.out.println(ANSI_PURPLE + dtf.format(now) + " - " + sql5 + ANSI_RESET); 
+                    statement5.executeUpdate();
+                    TimeUnit.SECONDS.sleep(3); 
+                    
+                    //Statement 3 INSERT
+                    now = LocalDateTime.now();  
+                    System.out.println(ANSI_PURPLE + dtf.format(now) + " - " + sql3 + ANSI_RESET); 
+                    statement3.executeUpdate();
+                    TimeUnit.SECONDS.sleep(10); 
                 }
             } else {
                 System.out.println("Couldn't connect to the Database!");
@@ -328,26 +438,45 @@ public class MonitorLogMSSQL {
             if (key_inputs.startsWith("Y")) {
                 writePropertiesFile(ip_address, port_number, instanceName, databaseName, username, password, username);
             }
-            
-            monitorLog(ip_address, port_number, instanceName, databaseName, username, password); 
-            
-            // Multithreading      
-//            class Runnable2 implements Runnable{
-//                public void run(){
-//                    monitorLog(ip_address, port_number, instanceName, databaseName, username, password); 
-//                }
-//            }
-//            class Runnable1 implements Runnable{
-//                public void run(){
-//                    testRandom(ANSI_RESET, ANSI_RESET, ANSI_RESET, ANSI_RESET, ANSI_BLUE, ANSI_RED);
-//                }
-//            }
-//            Runnable r = new Runnable1();
-//            Thread t = new Thread(r);
-//            Runnable r2 = new Runnable2();
-//            Thread t2 = new Thread(r2);
-//            t.start();
-//            t2.start(); 
+                             
+            System.out.print("Do you want to run program without testing function ? (Y/N): ");
+            key_inputs = sc.nextLine().toUpperCase();
+            if (key_inputs.startsWith("Y")) {
+                monitorLog(ip_address, port_number, instanceName, databaseName, username, password); 
+            } else {
+                // Multithreading     
+                final String final_ip_address = ip_address;
+                final String final_port_number = port_number;
+                final String final_instanceName = instanceName;
+                final String final_databaseName = databaseName;
+                final String final_username = username;
+                final String final_password = password;
+
+                System.out.println("\n" + ANSI_RED + "Starting Main Thread" + ANSI_RESET);
+
+                class Runnable1 implements Runnable{
+                    public void run(){
+                        monitorLog(final_ip_address, final_port_number, final_instanceName, final_databaseName, final_username, final_password); 
+                    }
+                }
+                Runnable r1 = new Runnable1();
+                Thread t1 = new Thread(r1);
+                t1.start();
+
+                // Waits 10 sec to start testing function
+                TimeUnit.SECONDS.sleep(10); 
+
+                System.out.println(ANSI_RED + "Starting random test." + ANSI_RESET);
+
+                class Runnable2 implements Runnable{
+                    public void run(){
+                        testRandom(final_ip_address, final_port_number, final_instanceName, final_databaseName, final_username, final_password);
+                    }
+                }
+                Runnable r2 = new Runnable2();
+                Thread t2 = new Thread(r2);
+                t2.start(); 
+            }
             
         } catch (Exception e) {
             e.printStackTrace();
